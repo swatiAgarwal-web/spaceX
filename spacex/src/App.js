2e171f2288ac7ff248da2f73f3ab7b40611631aa
Ravi <ravi@afternow.io>
2021-04-17 10:58:09 +0530

    implemented page refreshing with URL filter data

--- spacex/src/App.js
+++ spacex/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 import Home from "./component/Home";
+import {Route, Router, useLocation} from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 
-function App() {
+
+function App(props) {
+  const history = createMemoryHistory();
+  const {search} = useLocation();
+
   return (
     <div className="App">
-      <Home/>
+      <Router history={history}>
+          <Route path="/" exact render={() => ( <Home filter={search} /> )} />
+      </Router>
     </div>
   );
 }
